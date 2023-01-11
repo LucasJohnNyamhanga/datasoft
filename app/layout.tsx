f93151b02c5ddba022daf14174dcb0ed0f578fdf
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-01-11 14:11:59 +0300

    datasoft init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import './globals.css'
+import "./styles/globals.scss";
+import localFont from "@next/font/local";
+
+const montserrat = localFont({ src: "./font/Montserrat-Regular.woff2" });
+const roboto = localFont({ src: "./font/roboto-regular.woff2" });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${montserrat.className} ${roboto.className}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -14,5 +18,5 @@ export default function RootLayout({
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
