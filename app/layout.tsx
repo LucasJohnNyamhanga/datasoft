69857154824ba79049557f343b65f155657ec17c
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-04 17:15:37 +0300

    font optimasation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
 import "./styles/globals.scss";
-import localFont from "@next/font/local";
-
-const montserrat = localFont({ src: "./font/Montserrat-Regular.woff2" });
-
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className} ${montserrat.className}`}>
+    <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
