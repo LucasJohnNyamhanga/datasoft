f9ae4cec983b25a9c25f748820a0cd4f1d6181d3
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-09 12:42:02 +0300

    update footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Styles from "../styles/footer.module.scss";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.innerContainer}>
+        <div className={Styles.footerDetails}>
+          <div className={Styles.header}>MEET US</div>
+          <div className={Styles.body}>House No 13</div>
+          <div className={Styles.body}>Medical Research Road</div>
+          <div className={Styles.body}>Kitangiri, Ilemela</div>
+          <div className={Styles.body}>Mwanza, Tanzania</div>
+        </div>
+        <div className={Styles.footerDetails}>
+          <div className={Styles.header}>BRIEF US</div>
+          <div className={Styles.body}>{`Let's Talk Business`}</div>
+          <div className={Styles.body}>
+            <a href="tel:+255784477999">+255 784 477 999</a>
+          </div>
+          <div className={Styles.body}>
+            <a href="mailto:contact@datasoft.co.tz"> contact@datasoft.co.tz</a>
+          </div>
+        </div>
+        <div className={Styles.footerDetails}>
+          <div className={Styles.header}>SOCIAL MEDIA</div>
+          <div className={Styles.body}>
+            <div className={Styles.socialmenu}>
+              <FaInstagram className={Styles.media} />
+              <FaFacebookF className={Styles.media} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={Styles.copyrights}>
+        DataSoft Technologies Tanzania &copy; 2022
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
