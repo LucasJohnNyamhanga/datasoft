893ac45921211903b6aa6ea04a6976b096aaeb4a
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-09 13:11:46 +0300

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="DataSoft Technologies Tanzania" />
       <link rel="icon" href="/brainas.svg" />
+      <meta
+        name="google-site-verification"
+        content="oN-oJx51JrDZi5ZxU2E682HRJ0dyDB2ZEe9bGQQy3Ds"
+      />
     </>
   );
 }
