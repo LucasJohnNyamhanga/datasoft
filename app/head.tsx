d8e677e172cf65b36d8fd6e98368ba367d723153
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-11 09:51:32 +0300

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,28 @@
 export default function Head() {
   return (
     <>
-      <title>DataSoft</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="DataSoft Technologies Tanzania" />
-      <link rel="icon" href="/brainas.svg" />
       <meta
         name="google-site-verification"
         content="oN-oJx51JrDZi5ZxU2E682HRJ0dyDB2ZEe9bGQQy3Ds"
       />
+      <title>DataSoft Tanzania</title>
+      <link rel="icon" href="/brainas.svg" />
+      <meta
+        key="desc"
+        name="description"
+        content={`We are innovators, creators, technological makers. We turn ideas into full effecient working technological solution, that are customized to meet your company’s organizational needs, highlight its core competencies, and further its success.`}
+      />
+      {/* //!add keywords */}
+      <meta
+        name="keywords"
+        content={`We are innovators, creators, technological makers. We turn ideas into full effecient working technological solution, that are customized to meet your company’s organizational needs, highlight its core competencies, and further its success.`}
+      />
+      <meta
+        property="og:description"
+        content="We are innovators, creators, technological makers. We turn ideas into full effecient working technological solution, that are customized to meet your company’s organizational needs, highlight its core competencies, and further its success."
+      />
+      <meta property="og:image" content="/brainas.svg" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </>
   );
 }
