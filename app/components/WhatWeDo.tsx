85233e9c1b9fd79c2d3ecaaf4619597e1c6c7df0
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-13 11:35:57 +0300

    Update WhatWeDo.tsx

--- app/components/WhatWeDo.tsx
+++ app/components/WhatWeDo.tsx
@@ -14,74 +14,66 @@ const dataPoint = [
     id: 1,
     header: "Graphics Designing",
     icon: <FaImages />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
+    description: `We create artistic graphics works that intends to communicate your intended messages and objectives.`,
     link: `#`,
   },
   {
     id: 2,
-    header: "Website Development",
+    header: "Software Development",
     icon: <FaLaptopCode />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
+    description: `We design and build fully efficient working software solutions, that are customized to meet organizational needs.`,
     link: `#`,
   },
-  {
-    id: 3,
-    header: "Computer System Development",
-    icon: <FaDesktop />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
-    link: `#`,
-  },
-  {
-    id: 4,
-    header: "Mobile Applications Development",
-    icon: <FaGooglePlay />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
-    link: `#`,
-  },
-  {
-    id: 5,
-    header: "Database Development",
-    icon: <FaDatabase />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
-    link: `#`,
-  },
+  // {
+  //   id: 3,
+  //   header: "Computer System Development",
+  //   icon: <FaDesktop />,
+  //   description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
+  //               justo eu vulputate elementum, est augue tincidunt ante, sed
+  //               efficitur leo ligula vel velit.`,
+  //   link: `#`,
+  // },
+  // {
+  //   id: 4,
+  //   header: "Mobile Applications Development",
+  //   icon: <FaGooglePlay />,
+  //   description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
+  //               justo eu vulputate elementum, est augue tincidunt ante, sed
+  //               efficitur leo ligula vel velit.`,
+  //   link: `#`,
+  // },
+  // {
+  //   id: 5,
+  //   header: "Database Development",
+  //   icon: <FaDatabase />,
+  //   description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
+  //               justo eu vulputate elementum, est augue tincidunt ante, sed
+  //               efficitur leo ligula vel velit.`,
+  //   link: `#`,
+  // },
   {
     id: 6,
-    header: "Website Hosting",
+    header: "Hosting",
     icon: <FaGlobeAfrica />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
+    description: `We provide hosting services to systems, mobile applications and websites that have been built by our company. hosting services are free to our customers.`,
     link: `#`,
   },
   {
     id: 7,
     header: "Computer Networking",
     icon: <FaNetworkWired />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
+    description: `We create networks that connect computers to share resources such as printers and scanners, we make your office fully autonomous.`,
     link: `#`,
   },
-  {
-    id: 8,
-    header: "Software Installations",
-    icon: <FaMicrosoft />,
-    description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
-                justo eu vulputate elementum, est augue tincidunt ante, sed
-                efficitur leo ligula vel velit.`,
-    link: `#`,
-  },
+  // {
+  //   id: 8,
+  //   header: "Software Installations",
+  //   icon: <FaMicrosoft />,
+  //   description: `Mauris volutpat urna tristique finibus iaculis. Morbi facilisis,
+  //               justo eu vulputate elementum, est augue tincidunt ante, sed
+  //               efficitur leo ligula vel velit.`,
+  //   link: `#`,
+  // },
 ];
 
 const what = () => {
