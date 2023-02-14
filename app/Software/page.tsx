fbeac94f64589f57202c471c45a237168ef67fdf
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-02-14 13:40:03 +0300

    software development page

--- app/Software/page.tsx
+++ app/Software/page.tsx
@@ -1,11 +1,14 @@
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import GoToTop from "../components/GoToTop";
+import Hero from "../components/Hero";
+import styles from "../styles/page.module.scss";
 
 const Software = () => {
   return (
-    <div>
+    <div className={styles.main}>
       <Navigation />
+      <Hero image={"/datasoftsoftware.gif"} />
       <div>software</div>
       <Footer />
       <GoToTop />
