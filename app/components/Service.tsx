bafa1391d4d38c37c0dd0aa2cc3beabc70541645
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-06-16 13:31:44 +0300

    Update Service.tsx

--- app/components/Service.tsx
+++ app/components/Service.tsx
@@ -13,26 +13,24 @@ type dataType = {
 const Service = ({ header, icon, description, link }: dataType) => {
   return (
     <div className={styles.container}>
-      <Link href={link} className={styles.learnmore}>
-        {/* <!-- START SECTION SERVICES --> */}
-        <section className={styles.sectionservices}>
-          {/* <!-- Single Service --> */}
-          <div className={styles.cover}>
-            <div className={styles.singleservice}>
-              <div className={styles.content}>
-                <span className={styles.icon}>{icon}</span>
-                <h3 className={styles.title}>{header}</h3>
-                <p className={styles.description}>{description}</p>
-                <Link href={link} className={styles.learnmore}>
-                  Learn More
-                </Link>
-              </div>
-              <span className={styles.circlebefore}></span>
+      {/* <!-- START SECTION SERVICES --> */}
+      <div className={styles.sectionservices}>
+        {/* <!-- Single Service --> */}
+        <div className={styles.cover}>
+          <div className={styles.singleservice}>
+            <div className={styles.content}>
+              <span className={styles.icon}>{icon}</span>
+              <h3 className={styles.title}>{header}</h3>
+              <p className={styles.description}>{description}</p>
+              <Link href={link} className={styles.learnmore}>
+                Learn More
+              </Link>
             </div>
+            <span className={styles.circlebefore}></span>
           </div>
-          {/* <!-- / End Single Service --> */}
-        </section>
-      </Link>
+        </div>
+        {/* <!-- / End Single Service --> */}
+      </div>
     </div>
   );
 };
