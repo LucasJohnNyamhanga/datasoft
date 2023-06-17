598a5e317bab7e2097a7e1b5b555ab8e7bbb8687
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-06-17 16:09:37 +0300

    update call us

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -21,7 +21,7 @@ const Nav = () => {
         <div className={Styles.NavDetails}>
           <div className={Styles.topAdvatisment}>
             <p>
-              Call : <a href="tel:+255784477999">+255 784 477 999</a> or
+              Call Us: <a href="tel:+255784477999">+255 784 477 999</a> or
               <a href="#idea"> Fill Form Now</a>
             </p>
           </div>
