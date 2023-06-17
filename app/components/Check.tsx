5363f302ffdb751efe8bd6987701e8f723744a26
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-06-17 15:53:31 +0300

    Delete Check.tsx

--- app/components/Check.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSelectedLayoutSegment } from "next/navigation";
-
-export default function ExampleClientComponent() {
-  const segment = useSelectedLayoutSegment();
-
-  return <h1>Active segment: {segment}</h1>;
-}
